8a2f208946d743c0feb47d904ab0bf383e1a7b77
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-12 17:17:31 +0330

    edit to add header cmp

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,19 +2,17 @@ import { Link, withRouter } from "react-router-dom";
 
 const Navigation = (props) => {
   console.log(props);
-    return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">home</Link>
-          </li>
-          <li>
-            <Link to="/about-us">About-Us</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">home</Link>
+        </li>
+        <li>
+          <Link to="/about-us">About-Us</Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
