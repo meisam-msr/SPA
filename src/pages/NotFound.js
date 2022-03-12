a3baaa37a51df48ca2552d8de0d14255a7917b1f
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-12 19:11:18 +0330

    edit 404 page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function NotFound(props) {
   return (
     <>
-      <p> NotFound ...!!!</p>
+      <p>
+        404
+        <br />
+        the page not found
+      </p>
+      <Link to="/">go to home page</Link>
     </>
   );
 }
