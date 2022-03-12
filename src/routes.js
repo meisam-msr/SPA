ba1cf8e2c1d0db01c01528410dd5ba4b3519ea65
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-12 22:44:11 +0330

    add blogs routes

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,17 @@
 import AboutUs from "./pages/AboutUsPage";
+import BlogPage from "./pages/BlogPage";
 import HomePage from "./pages/HomePage";
 import NotFound from "./pages/NotFound";
 import Profile from "./pages/Profile";
+import Blog from "./components/Blog";
 
 const routes = [
-  { path: "/", component: HomePage, exact: true },
+  { path: "/blogs/:id", component: Blog },
   { path: "/profile", component: Profile },
   { path: "/about-us", component: AboutUs },
+  { path: "/blogs", component: BlogPage },
   { component: NotFound },
+  { path: "/", component: HomePage, exact: true },
 ];
 
 export default routes;
