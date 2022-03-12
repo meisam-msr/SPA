a122de4cb6df393b4b0ee5b23f3b50eb041e939e
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-12 17:41:18 +0330

    create profile page

---
+++ src/pages/Profile.js
@@ -0,0 +1,11 @@
+import Layout from "../Layout/Layout";
+
+const Profile = () => {
+  return (
+    <Layout>
+      <p>wellcome back ! dear Meisam !</p>
+    </Layout>
+  );
+};
+
+export default Profile;
