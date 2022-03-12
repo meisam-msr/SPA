8f24fef5baf3809f93dab58aa3745c3cea623f0b
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-12 14:20:22 +0330

    create pages

---
+++ src/pages/AboutUsPage.js
@@ -0,0 +1,9 @@
+const AboutUsPage = () => {
+  return (
+    <>
+      <p>اینجا صفحه درباره ماست</p>
+    </>
+  );
+};
+
+export default AboutUsPage;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <>
+      <p>اینجا صفحه خانه است</p>
+    </>
+  );
+};
+
+export default HomePage;
