02e801d0806f2165367c59e44e69716ca28740ed
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-12 17:51:14 +0330

    add items to dynamication list

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,28 @@
-import { Link, withRouter } from "react-router-dom";
+import { NavLink, withRouter } from "react-router-dom";
 
-const Navigation = (props) => {
+const items = [
+  { name: "Home", to: "/", exact: true },
+  { name: "About-us", to: "/about-us" },
+  { name: "Profile", to: "/profile" },
+];
+
+const Navigation = (location) => {
   return (
     <nav>
       <ul>
-        <li>
-          <Link to="/">home</Link>
-        </li>
-        <li>
-          <Link to="/about-us">About-Us</Link>
-        </li>
+        {items.map((item) => {
+          return (
+            <li key={item.to}>
+              <NavLink
+                to={item.to}
+                activeClassName="activeLink"
+                exact={item.exact || false}
+              >
+                {item.name}
+              </NavLink>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
