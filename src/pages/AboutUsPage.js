9b63ddab2edb450b01ecdf9661d0143daae2d419
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-12 17:01:55 +0330

    add link to page cmp

--- src/pages/AboutUsPage.js
+++ src/pages/AboutUsPage.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function AboutUsPage(props) {
+function AboutUs(props) {
   return (
     <>
       <p>اینجا صفحه درباره ماست</p>
+      <Link to="/">go to HomePage</Link>
     </>
   );
 }
 
-export default AboutUsPage;
+export default AboutUs;
