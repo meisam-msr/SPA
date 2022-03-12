a7d4ca681806446f1704e841716f7e098c05b760
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-12 19:11:00 +0330

    add 404 page to routes array

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import AboutUs from "./pages/AboutUsPage";
 import HomePage from "./pages/HomePage";
+import NotFound from "./pages/NotFound";
 import Profile from "./pages/Profile";
 
 const routes = [
   { path: "/", component: HomePage, exact: true },
   { path: "/profile", component: Profile },
   { path: "/about-us", component: AboutUs },
+  { component: NotFound },
 ];
 
 export default routes;
