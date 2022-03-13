128d7276dbba25e1d8b50b44a1e95ca00b8b164e
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-13 17:03:18 +0330

    get query string

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link} from "react-router-dom";
+import queryString from "query-string";
 
-const Blog = (props) => {
-  const id = props.match.params.id;
+const Blog = ({ location, match }) => {
+  // use query string lib
+  const query = queryString.parse(location.search);
+  console.log(query);
+  const id = match.params.id;
   return (
     <div>
       <h2>Blog detail - {id}</h2>
