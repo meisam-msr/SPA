422d9aaacf0647304710c9dcedfb1ec2855ab523
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-13 15:54:53 +0330

    add optional post page route

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,14 @@ import HomePage from "./pages/HomePage";
 import NotFound from "./pages/NotFound";
 import Profile from "./pages/Profile";
 import Blog from "./components/Blog";
+import PostsPage from "./pages/PostsPage";
 
 const routes = [
   { path: "/blogs/:id", component: Blog },
   { path: "/profile", component: Profile },
   { path: "/about-us", component: AboutUs },
   { path: "/blogs", component: BlogPage },
+  { path: "/posts/:id([0-9]+)?", component: PostsPage },
   { component: NotFound },
   { path: "/", component: HomePage, exact: true },
 ];
