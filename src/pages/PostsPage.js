b54e56f0e51b76ffbfa3928265c0c2d9e9e4578f
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-13 15:53:54 +0330

    create post page

---
+++ src/pages/PostsPage.js
@@ -0,0 +1,14 @@
+const PostsPage = (props) => {
+  const id = props.match.params.id || 1;
+  console.log(props.match.params);
+
+  return (
+    <div>
+      <h2>Post detail - {id}</h2>
+      <div>{JSON.stringify(props.match.params)}</div>
+    </div>
+  );
+};
+
+export default PostsPage;
+ 
