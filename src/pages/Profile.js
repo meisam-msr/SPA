ae23ef665798720568005850bae7bdd87f0afe93
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-13 19:29:16 +0330

    add routes to profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,19 @@
+import { Route } from "react-router-dom";
+import SideBar from "../components/SideBar";
+import DashBoard from "../components/DashBoard";
+import Downloads from "../components/Downloads";
+
 const Profile = () => {
-  return <p>wellcome back ! dear Meisam !</p>;
+  return (
+    <>
+      <h2 style={{ margin: "30px" }}>wellcome back ! dear Meisam !</h2>
+      <div className="sideBar">
+        <SideBar />
+        <Route path="/profile/dashboard" component={DashBoard} />
+        <Route path="/profile/downloads" component={Downloads} />
+      </div>
+    </>
+  );
 };
 
 export default Profile;
